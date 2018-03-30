792c3ce5838b72f1d7741d2414e072a6ff004154
Mike Hearn <mike@plan99.net>
2018-03-30 12:49:08 +0200

    Initial commit

---
+++ website/make-docsite.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# The purpose of this file is to make the docsite in a python virtualenv
+# You can call it manually if running make manually, otherwise gradle will run it for you
+
+# Activate the virtualenv
+if [ -d "virtualenv/bin" ]
+then
+    # it's a Unix system
+    source virtualenv/bin/activate
+else
+    source virtualenv/Scripts/activate
+fi
+
+# TODO: The PDF rendering is pretty ugly and can be improved a lot.
+# make pdf
+# mv build/pdf/graviton-design.pdf build/html/_static/graviton-design.pdf
+make html
