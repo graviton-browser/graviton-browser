bf13a34ee8d72b817cf3e8e3819aa11022e0e624
Anindya Chatterjee <anidotnet@gmail.com>
2018-04-03 23:21:20 +0530

    Follow linux convention for cache directory

--- shell/src/main/kotlin/net/plan99/graviton/Utils.kt
+++ shell/src/main/kotlin/net/plan99/graviton/Utils.kt
@@ -17,7 +17,13 @@ enum class OperatingSystem {
     },
     LINUX {
         override val appCacheDirectory: Path
-            get() = Paths.get(System.getProperty("user.home"), ".graviton", "Cache")
+            get() {
+                return if (System.getenv("XDG_CACHE_HOME").isNullOrBlank()) {
+                    Paths.get(System.getProperty("user.home"), ".cache", "GravitonBrowser")
+                } else {
+                    Paths.get(System.getenv("XDG_CACHE_HOME"), "GravitonBrowser")
+                }
+            }
     },
     UNKNOWN {
         override val appCacheDirectory: Path
